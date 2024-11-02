fd9383b3074a265dc052135afcc9c423cdaedd81
PikalovAndrey <andrtan10mail.com@gmail.com>
2024-11-02 16:03:18 +0200

    improved Linter

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
+
+import App from './App';
 import './main.scss';
 
 createRoot(document.getElementById('root')!).render(<App />);
