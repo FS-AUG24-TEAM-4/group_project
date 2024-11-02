44dadbcb271e11eae05fe9e74ddd1bec3b5fd8bf
PikalovAndrey <andrtan10mail.com@gmail.com>
2024-11-02 17:54:45 +0200

    solved problem with autodeploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/GROUP_PROJECT/',
+  base: '/group_project/',
   server: {
     port: 3000,
     open: true,
