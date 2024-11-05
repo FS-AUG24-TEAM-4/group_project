1bd6be0b73a28e320f898f8581e99a1e6b4c2ac9
Anna Maksai <annamaksai99@gmail.com>
2024-11-05 21:24:29 +0200

    fix: link< and border

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -11,7 +11,7 @@ import styles from './styles.module.scss';
 export const Header = () => {
     return <header className={styles.header}>
         <div className={styles.navContainer}>
-            <Link to='/' className={styles.logo}>
+            <Link to={Paths.HOME} className={styles.logo}>
                 <img src={logo} alt='Nice gadgets logo' className={styles.logoImage}></img>
             </Link>
             <Navigation links={HeaderNavigationLinks}/>
