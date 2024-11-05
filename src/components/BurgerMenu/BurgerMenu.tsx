9d1b67d2185b260c2762b911ed4800697d0976fd
Yevhenii Melnyk <rpm.steel@gmail.com>
2024-11-05 17:44:45 +0200

    some fixes

--- src/components/BurgerMenu/BurgerMenu.tsx
+++ src/components/BurgerMenu/BurgerMenu.tsx
@@ -10,65 +10,77 @@ import cart from '../../assets/icons/shop-cart-icon.png';
 
 export const BurgerMenu = () => {
   return (
-    <>
-    <menu className={styles.burger}>
-    <div className={styles.container}>
-      <header className={styles.burger__header}>
-        <a className={styles.burger__header__img} href="/group_project/">
-          <img
-            className={styles.burger__logo}
-            src={logo}
-            alt="nice-gadgets_logo"
-          />
-        </a>
+      <menu className={styles.burger}>
+        <div className={styles.container}>
+          <header className={styles.burger__header}>
+            <a className={styles.burger__header__img} href="/group_project/">
+              <img
+                className={styles.burger__logo}
+                src={logo}
+                alt="nice-gadgets_logo"
+              />
+            </a>
 
-        <div className={styles.burger__header__button}>
-          <img
-            className={styles.burger__header__button__icon}
-            src={close_menu_icon}
-            alt="close-menu-icon"
-          />
+            <div className={styles.burger__header__button}>
+              <img
+                className={styles.burger__header__button__icon}
+                src={close_menu_icon}
+                alt="close-menu-icon"
+              />
+            </div>
+          </header>
+
+          <nav className={styles.burger__nav}>
+            <a className={styles.burger__nav__links__active} href="/">
+              Home
+            </a>
+
+            <a
+              className={styles.burger__nav__links}
+              href="/group_project/phones"
+            >
+              Phones
+            </a>
+
+            <a
+              className={styles.burger__nav__links}
+              href="/group_project/tablets"
+            >
+              Tablets
+            </a>
+
+            <a
+              className={styles.burger__nav__links}
+              href="/group_project/accessories"
+            >
+              Accessories
+            </a>
+          </nav>
         </div>
-      </header>
 
-      <nav className={styles.burger__nav}>
-        <a className={styles.burger__nav__links__active} href="/">
-          Home
-        </a>
+        <footer className={styles.burger__footer}>
+          <a
+            href="/group_project/fav"
+            className={styles.burger__footer__links__active}
+          >
+            <img
+              className={styles.burger__footer__links__fav}
+              src={fav}
+              alt="fav-icon"
+            />
+          </a>
 
-        <a className={styles.burger__nav__links} href="/group_project/phones">
-          Phones
-        </a>
-
-        <a className={styles.burger__nav__links} href="/group_project/tablets">
-          Tablets
-        </a>
-
-        <a className={styles.burger__nav__links} href="/group_project/accessories">
-          Accessories
-        </a>
-      </nav>
-    </div>
-
-    <footer className={styles.burger__footer}>
-      <a href="/group_project/fav" className={styles.burger__footer__links__active}>
-        <img
-          className={styles.burger__footer__links__fav}
-          src={fav}
-          alt="fav-icon"
-        />
-      </a>
-
-      <a href="/group_project/cart" className={styles.burger__footer__links}>
-        <img
-          className={styles.burger__footer__links__cart}
-          src={cart}
-          alt="cart-icon"
-        />
-      </a>
-    </footer>
-  </menu>
-    </>
-    
+          <a
+            href="/group_project/cart"
+            className={styles.burger__footer__links}
+          >
+            <img
+              className={styles.burger__footer__links__cart}
+              src={cart}
+              alt="cart-icon"
+            />
+          </a>
+        </footer>
+      </menu>
   );
 };
