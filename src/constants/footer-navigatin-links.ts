7d6e6f7214a954781eb277cbc2cf9239c1961127
Anna Maksai <annamaksai99@gmail.com>
2024-11-06 09:14:21 +0200

    style: linter fixes

--- src/constants/footer-navigatin-links.ts
+++ src/constants/footer-navigatin-links.ts
@@ -1,6 +1,6 @@
 export const FooterNavigationLinks = [
-    {
-        title: 'Github',
-        route: 'https://github.com/FS-AUG24-TEAM-4/group_project',
-    }
-]
+  {
+    title: 'Github',
+    route: 'https://github.com/FS-AUG24-TEAM-4/group_project',
+  },
+];
--- src/constants/header-navigatin-links.ts
+++ src/constants/header-navigatin-links.ts
@@ -1,20 +1,20 @@
-import { Paths } from '@/enums'
+import { Paths } from '@/enums';
 
 export const HeaderNavigationLinks = [
-    {
-        title: 'Home',
-        route: Paths.HOME,
-    },
-    {
-        title: 'Phones',
-        route: Paths.PHONES,
-    },
-    {
-        title: 'Tablets',
-        route: Paths.TABLETS,
-    },
-    {
-        title: 'Accessories',
-        route: Paths.ACCESSORIES,
-    }
-]
+  {
+    title: 'Home',
+    route: Paths.HOME,
+  },
+  {
+    title: 'Phones',
+    route: Paths.PHONES,
+  },
+  {
+    title: 'Tablets',
+    route: Paths.TABLETS,
+  },
+  {
+    title: 'Accessories',
+    route: Paths.ACCESSORIES,
+  },
+];
