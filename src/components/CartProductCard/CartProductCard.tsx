4cd2eb54762df266dd815829c42cc5a2244b4d39
PikalovAndrey <andrtan10mail.com@gmail.com>
2024-11-06 13:46:40 +0200

    started doing CartProductCard

--- src/components/CartProductCard/CartProductCard.tsx
+++ src/components/CartProductCard/CartProductCard.tsx
@@ -1,22 +1,17 @@
 import { Phone } from '@/types/Phone';
 import { FC, useState } from 'react';
-import { PrimaryButton } from '../PrimaryButton';
-import { PrimaryButtons } from '@/enums';
-import { FavoritesButton } from '../FavoritesButton/FavoritesButton';
 import styles from './styles.module.scss';
-import blankIcon from '../../assets/images/icons/favorites-blank.svg';
-import filledIcon from '../../assets/images/icons/favorites-filled.svg';
 
 interface INCartProductCard {
   phone: Phone;
 }
 
 export const CartProductCard: FC<INCartProductCard> = ({ phone }) => {
-  const [clickedBuy, setClickedBuy] = useState(false);
-  const [clickedFav, setClickedFav] = useState(false);
+  const [count, setCount] = useState(1);
 
   return (
     <article className={styles.card}>
+      <button className={styles.deleteButton}></button>
       <a href="#">
         <img
           src={phone.images[0]}
@@ -24,55 +19,25 @@ export const CartProductCard: FC<INCartProductCard> = ({ phone }) => {
           className={styles.picture}
         />
       </a>
-
       <a href="#">
         <h2 className={styles.title}>{phone.name}</h2>
       </a>
 
-      <div className={styles.price}>
-        <p className={styles.actual_price}>
-          ${phone.priceDiscount || phone.priceRegular}
-        </p>
-        {phone.priceDiscount && (
-          <p className={styles.old_price}>${phone.priceRegular}</p>
-        )}
-      </div>
-
-      <div className={styles.line}></div>
-      <div className={styles.specs}>
-        <p className={styles.label}>Screen</p>
-        <p className={styles.value}>{phone.screen.replace(`'`, `''`)}</p>
-      </div>
-
-      <div className={styles.specs}>
-        <p className={styles.label}>Capacity</p>
-        <p className={styles.value}>{phone.capacity.replace('GB', '')} GB</p>
-      </div>
-
-      <div className={styles.specs}>
-        <p className={styles.label}>RAM</p>
-        <p className={styles.value}>{phone.ram.replace('GB', '')} GB</p>
-      </div>
-
       <div className={styles.buttons}>
-        <PrimaryButton
-          type={PrimaryButtons.CART}
-          onClick={() => setClickedBuy(!clickedBuy)}
-          isActive={clickedBuy}
-        >
-          {clickedBuy ? 'Added' : 'Add to cart'}
-        </PrimaryButton>
-
-        <FavoritesButton
-          onClick={() => setClickedFav(!clickedFav)}
-          isActive={clickedFav}
-        >
-          <img
-            src={clickedFav ? filledIcon : blankIcon}
-            alt="Add to favorites"
-          />
-        </FavoritesButton>
+        <button
+          className={styles.decrease}
+          onClick={() => setCount(prev => prev - 1)}
+        ></button>
+        <span>{count}</span>
+        <button
+          className={styles.increase}
+          onClick={() => setCount(prev => prev + 1)}
+        ></button>
       </div>
+
+      <p className={styles.actual_price}>
+        ${phone.priceDiscount || phone.priceRegular}
+      </p>
     </article>
   );
 };
