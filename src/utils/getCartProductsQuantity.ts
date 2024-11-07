d85c3f6c604ee934ab6a11f07d68078b35d2c4ca
Yevhenii Melnyk <rpm.steel@gmail.com>
2024-11-07 17:23:25 +0200

    previous but with all changes

--- src/utils/getCartProductsQuantity.ts
+++ src/utils/getCartProductsQuantity.ts
@@ -1,4 +1,4 @@
-import { CartItem } from '@/features/Cart/CartSlice';
+import { CartItem } from '@/types/cartItem';
 
 export const getCartProductsQuantity = (products: CartItem[]) => {
   return products.reduce((acc, product) => acc + product.quantity, 0);
