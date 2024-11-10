64bd311b5d9cf3cb264a5a0319f7fc00c528a1a0
Vasyliazhenko Dmytro Vitaliyovych <demon4225@gmail.com>
2024-11-10 21:05:57 +0200

    Moved function to utils

--- src/components/HotPricesSwiper/HotPricesSwiper.tsx
+++ src/components/HotPricesSwiper/HotPricesSwiper.tsx
@@ -1,31 +1,14 @@
 import { FC } from 'react';
 import { Product } from '@/types/Product';
 import { ProductSlider } from '../ProductSlider/ProductSlider';
-import { DeviceCategory } from '@/enums';
+import { getTopDiscountProducts } from '@/utils/getTopDiscountProduct';
 
 interface ProductSliderProps {
   products: Product[];
 }
 
 export const HotPricesSwiper: FC<ProductSliderProps> = ({ products }) => {
-  function getTopDiscountProductsByCategory(productsArr: Product[]): Product[] {
-    const getDiscount = (product: Product) => product.fullPrice - product.price;
-
-    const topProducts: Product[] = [];
-
-    Object.values(DeviceCategory).forEach(category => {
-      const topInCategory = [...productsArr]
-        .filter(product => product.category === category)
-        .sort((a, b) => getDiscount(b) - getDiscount(a))
-        .slice(0, 5);
-
-      topProducts.push(...topInCategory);
-    });
-
-    return topProducts.sort(() => Math.random() - 0.5);
-  }
-
-  const BestDiscountProducts = getTopDiscountProductsByCategory(products);
+  const BestDiscountProducts = getTopDiscountProducts(products);
 
   return <ProductSlider products={BestDiscountProducts} title="Hot Prices" />;
 };
