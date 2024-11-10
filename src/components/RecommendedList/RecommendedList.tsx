d8d46c0626667f63f3169f639691b0dfd04d0a2d
Anna Maksai <annamaksai99@gmail.com>
2024-11-10 16:18:13 +0200

    feat: recommended list

--- src/components/RecommendedList/RecommendedList.tsx
+++ src/components/RecommendedList/RecommendedList.tsx
@@ -1,29 +1,31 @@
-// import { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 
-// import { Product } from '@/types';
-// import { useProducts } from '@/hooks/useProducts';
-// import { getRecommendedProducts } from '@/utils';
+import { Product } from '@/types';
+import { useProducts } from '@/hooks/useProducts';
+import { getRecommendedProducts } from '@/utils';
 
-// import { Slider } from '../Slider';
+import { ProductSlider } from '../ProductSlider';
 
 interface Props {
   price: number;
   category: string;
 }
 
-export const RecommendedList: React.FC<Props> = () => {
-  // const { products } = useProducts();
+export const RecommendedList: React.FC<Props> = ({ price, category }) => {
+  const { products } = useProducts();
 
-  // const [recommendedProducts, setRecommendedProducts] = useState<Product[]>([]);
+  const [recommendedProducts, setRecommendedProducts] = useState<Product[]>([]);
 
-  // useEffect(() => {
-  //   setRecommendedProducts(getRecommendedProducts(products, price, category));
-  // }, [price, category, products]);
+  useEffect(() => {
+    setRecommendedProducts(getRecommendedProducts(products, price, category));
+  }, [price, category, products]);
 
   return (
     <section>
-      <h4>You may also like</h4>
-      {/* <Slider products={recommendedProducts}></Slider> */}
+      <ProductSlider
+        title="You may also like"
+        products={recommendedProducts}
+      ></ProductSlider>
     </section>
   );
 };
